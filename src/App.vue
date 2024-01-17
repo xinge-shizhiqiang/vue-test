75db12941eb71ef84ecf8503652c8d795839d332
shizhiqiang <912034159@qq.com>
2024-01-17 16:12:46 +0800

    123123121

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to SZQ!!!"/>
+    <HelloWorld msg="Welcome!!!"/>
   </div>
 </template>
 
